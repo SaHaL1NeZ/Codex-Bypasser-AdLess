bba092fa3712c9da62e1d3e9853af881e441aff2
SaHaL1NeZ <sz.1mperative@gmail.com>
2025-07-09 05:23:01 +0500

    Text fixed, icon added

--- Codex Bypasser AdLess.js
+++ Codex Bypasser AdLess.js
@@ -1,44 +1,45 @@
 // ==UserScript==
 // @name         Codex Bypasser AdLess
 // @description  Automatically bypasses Codex's key-system.
-// @author       SaHaL1NeZ (original: idontgiveaf, d15c0rdh4ckr (768868463459434517))
+// @author       SaHaL1NeZ (original: idontgiveaf; d15c0rdh4ckr (768868463459434517))
+// @namespace    https://greasyfork.org/users/1337278
 // @match        https://mobile.codex.lol/*
 // @connect      linkvertise.com
 // @connect      api.codex.lol
 // @connect      *
-// @version      1.1
+// @version      1.2
+// @icon         https://www.google.com/s2/favicons?sz=32&domain=codex.lol
 // @run-at       document-end
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
-// @namespace https://greasyfork.org/users/1337278
 // ==/UserScript==
 Promise.resolve(true)
   .then(data => {
-if (document.title == 'Just a moment...') {
+if (document.title=='Waiting for magic to start...') {
     return;
 }
  (() => {
- 	var __webpack_modules__ = ({
+ 	var __webpack_modules__=({
  24:
  ((module, __unused_webpack_exports, __webpack_require__) => {
-const { handleError, sleep, linkvertiseSpoof, getTurnstileResponse, getGrecaptchaResponse, notification, base64decode } = __webpack_require__(223)
+const { handleError, sleep, linkvertiseSpoof, getTurnstileResponse, getGrecaptchaResponse, notification, base64decode }=__webpack_require__(223)
 async function codex() {
     let session;
     while (!session) {
-        session = localStorage.getItem("android-session");
+        session=localStorage.getItem("android-session");
         await sleep(1000);
     }
     if (document?.getElementsByTagName('a')?.length && document.getElementsByTagName('a')[0].innerHTML.includes('Get started')) {
         document.getElementsByTagName('a')[0].click();
     }
     async function getStages() {
-        let response = await fetch('https://api.codex.lol/v1/stage/stages', {
+        let response=await fetch('https://api.codex.lol/v1/stage/stages', {
             method: 'GET',
             headers: {
                 'Android-Session': session
             }
         });
-        let data = await response.json();
+        let data=await response.json();
         if (data.success) {
             if (data.authenticated) {
                 return [];
@@ -50,7 +51,7 @@ async function codex() {
         }
     }
     async function initiateStage(stageId) {
-        let response = await fetch('https://api.codex.lol/v1/stage/initiate', {
+        let response=await fetch('https://api.codex.lol/v1/stage/initiate', {
             method: 'POST',
             headers: {
                 'Android-Session': session,
@@ -58,7 +59,7 @@ async function codex() {
             },
             body: JSON.stringify({ stageId })
         });
-        let data = await response.json();
+        let data=await response.json();
         if (data.success) {
             return data.token;
         }
@@ -67,7 +68,7 @@ async function codex() {
         }
     }
     async function validateStage(token, referrer) {
-        let response = await fetch('https://api.codex.lol/v1/stage/validate', {
+        let response=await fetch('https://api.codex.lol/v1/stage/validate', {
             method: 'POST',
             headers: {
                 'Android-Session': session,
@@ -76,7 +77,7 @@ async function codex() {
             },
             body: JSON.stringify({ token })
         });
-        let data = await response.json();
+        let data=await response.json();
         if (data.success) {
             return data.token;
         }
@@ -85,7 +86,7 @@ async function codex() {
         }
     }
     async function authenticate(validatedTokens) {
-        let response = await fetch('https://api.codex.lol/v1/stage/authenticate', {
+        let response=await fetch('https://api.codex.lol/v1/stage/authenticate', {
             method: 'POST',
             headers: {
                 'Android-Session': session,
@@ -93,7 +94,7 @@ async function codex() {
             },
             body: JSON.stringify({ tokens: validatedTokens })
         });
-        let data = await response.json();
+        let data=await response.json();
         if (data.success) {
             return true;
         }
@@ -102,43 +103,43 @@ async function codex() {
         }
     }
     function decodeTokenData(token) {
-        let data = token.split(".")[1];
-        data = base64decode(data);
+        let data=token.split(".")[1];
+        data=base64decode(data);
         return JSON.parse(data);
     }
-    let stages = await getStages();
-    let stagesCompleted = 0;
+    let stages=await getStages();
+    let stagesCompleted=0;
     while (localStorage.getItem(stages[stagesCompleted]) && stagesCompleted < stages.length) {
         stagesCompleted++;
     }
-    if (stagesCompleted == stages.length) {
+    if (stagesCompleted==stages.length) {
         return;
     }
-    let validatedTokens = [];
+    let validatedTokens=[];
     try {
         while (stagesCompleted < stages.length) {
-            let stageId = stages[stagesCompleted].uuid;
-            let initToken = await initiateStage(stageId);
+            let stageId=stages[stagesCompleted].uuid;
+            let initToken=await initiateStage(stageId);
             await sleep(6000);
-            let tokenData = decodeTokenData(initToken);
+            let tokenData=decodeTokenData(initToken);
             let referrer;
             if (tokenData.link.includes('loot-links')) {
-                referrer = 'https://loot-links.com/';
+                referrer='https://loot-links.com/';
             }
             else if (tokenData.link.includes('loot-link')) {
-                referrer = 'https://loot-link.com/';
+                referrer='https://loot-link.com/';
             }
             else {
-                referrer = 'https://linkvertise.com/';
+                referrer='https://linkvertise.com/';
             }
-            let validatedToken = await validateStage(initToken, referrer);
+            let validatedToken=await validateStage(initToken, referrer);
             validatedTokens.push({ uuid: stageId, token: validatedToken });
             notification(`${stagesCompleted + 1}/${stages.length} stages completed`, 5000);
             await sleep(1500);
             stagesCompleted++;
         }
         if (authenticate(validatedTokens)) {
-            notification('Bypass success :3');
+            notification('Bypass succeed');
             await sleep(3000);
             window.location.reload();
         }
@@ -147,14 +148,14 @@ async function codex() {
         handleError(e);
     }
 }
-module.exports = {
+module.exports={
     codex,
 }
  }),
  223:
  ((module) => {
 function handleError(error) {
-    let errorText = error.message ? error.message : error;
+    let errorText=error.message ? error.message : error;
     alert(errorText);
     GM_notification({
         text: errorText,
@@ -184,11 +185,11 @@ function linkvertiseSpoof(link) {
     });
 }
 async function getTurnstileResponse() {
-    let notif = setInterval(notification, 6000, 'please solve the captcha', 5000)
-    let res = '';
+    let notif=setInterval(notification, 6000, 'Please solve the captcha', 5000)
+    let res='';
     while (true) {
         try {
-            res = turnstile.getResponse();
+            res=turnstile.getResponse();
             if (res) { break; }
         } catch (e) { }
         await sleep(1000);
@@ -197,11 +198,11 @@ async function getTurnstileResponse() {
     return turnstile.getResponse();
 }
 async function getGrecaptchaResponse() {
-    let notif = setInterval(notification, 6000, 'please solve the captcha', 5000)
-    let res = '';
+    let notif=setInterval(notification, 6000, 'Please solve the captcha', 5000)
+    let res='';
     while (true) {
         try {
-            res = grecaptcha.getResponse();
+            res=grecaptcha.getResponse();
             if (res) { break; }
         } catch (e) { }
         await sleep(1000);
@@ -210,19 +211,19 @@ async function getGrecaptchaResponse() {
     return grecaptcha.getResponse();
 }
 function notification(message, timeout) {
-    let config = {
+    let config={
         text: message,
         title: "INFO",
         silent: true,
     }
-    if (timeout) { config.timeout = timeout; }
+    if (timeout) { config.timeout=timeout; }
     GM_notification(config);
 }
 function base64decode(str) {
-    str = str.replace(/-/g, '+').replace(/_/g, '/');
+    str=str.replace(/-/g,'+').replace(/_/g,'/');
     return atob(str);
 }
-module.exports = {
+module.exports={
     handleError,
     sleep,
     linkvertiseSpoof,
@@ -233,13 +234,13 @@ module.exports = {
 }
  })
  	});
- 	var __webpack_module_cache__ = {};
+ 	var __webpack_module_cache__={};
  	function __webpack_require__(moduleId) {
- 		var cachedModule = __webpack_module_cache__[moduleId];
+ 		var cachedModule=__webpack_module_cache__[moduleId];
  		if (cachedModule !== undefined) {
  			return cachedModule.exports;
  		}
- 		var module = __webpack_module_cache__[moduleId] = {
+ 		var module=__webpack_module_cache__[moduleId]={
  			exports: {}
  		};
 
@@ -247,25 +248,25 @@ module.exports = {
 
  		return module.exports;
  	}
-var __webpack_exports__ = {};
+var __webpack_exports__={};
 (() => {
-const { codex } = __webpack_require__(24);
-const { sleep, notification } = __webpack_require__(223);
+const { codex }=__webpack_require__(24);
+const { sleep, notification }=__webpack_require__(223);
 start();
 async function start() {
     if (window.location.hostname != 'keyrblx.com') {
         alert('Bypassing started');
     }
     GM_notification({
-        text: 'Do NOT touch anything. It will do it automatically.',
-        title: "Alert!",
+        text: 'Do NOT touch anything',
+        title: "Alert",
         silent: true,
         timeout: 5000
     });
     await sleep(6000);
     GM_notification({
-        text: `Bypass started, wait please.`,
-        title: "Alert!",
+        text: `Bypass started`,
+        title: "Alert",
         silent: true,
         timeout: 2000
     });
